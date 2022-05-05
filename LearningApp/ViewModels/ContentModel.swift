36f3ba4885552528977794fa6bb35c4512916a72
Brent <brenton.crowley@gmail.com>
2022-05-05 17:01:03 +1000

    parse style data in content model

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -11,10 +11,12 @@ class ContentModel: ObservableObject {
     
     @Published private(set) var modules:[Module] = []
     
+    private var styleData:Data?
+    
     init() {
         
         self.modules = getItemsFromJSONFile("data", type: Module.self) ?? []
-        
+        self.styleData = getStyleDataHTMLFile("style")
     }
     
     func getItemsFromJSONFile<T>(_ filename:String, type:T.Type) -> [T]? where T:Decodable {
@@ -29,6 +31,14 @@ class ContentModel: ObservableObject {
         guard let items = try? decoder.decode([T].self, from: data) else { return nil }
         
         return items
+    }
+    
+    func getStyleDataHTMLFile(_ filename:String) -> Data? {
+        
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "html") else { return nil}
+        guard let data = try? Data(contentsOf: url) else { return nil }
+        
+        return data
     }
     
 }
